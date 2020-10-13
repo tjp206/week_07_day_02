7f7fe7adb8e8ea72131ca33746b135844951ddd8
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-10-13 10:43:22 +0100

    first commit

---
+++ fetch_promises_lifecycle_hooks/fetch_start_point/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>RANDOGMISER</h1>
+    <img id="dog-img" v-if="dogImgURL" :src="dogImgURL">
+    <button v-on:click="fetchDog">Gimme those dogs!</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      dogImgURL: null
+    }
+  },
+  methods: {
+    fetchDog: function(){
+      console.log('Hello from fetchDog!');
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 40%;
+}
+
+#dog-img {
+  width: 500px;
+}
+
+</style>
